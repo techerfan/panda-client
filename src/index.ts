34ae84968d9aea855641adfd7679148579e4bbb6
techerfan <techerfan@gmail.com>
2021-09-19 13:04:43 +0430

    make config optional

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { Socket } from './Socket';
 
 export interface Config {
-  autoReconnect: boolean;
-  onClose: (event: Event) => void;
-  onError: (err: Event) => void;
-  onOpen: (event: Event) => void;
+  autoReconnect?: boolean;
+  onClose?: (event: Event) => void;
+  onError?: (err: Event) => void;
+  onOpen?: (event: Event) => void;
 }
 
-export const panda = (path: string, cfg: Config): Socket => {
-  return new Socket(path, cfg);
+export const panda = (path: string, cfg?: Config): Socket => {
+  if (cfg) {
+    return new Socket(path, cfg);
+  } else {
+    return new Socket(path, {});
+  }
 };
