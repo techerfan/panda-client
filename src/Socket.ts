2475898e3bfca018b845f161fb898daf8b90bd29
techerfan <techerfan@gmail.com>
2021-09-19 12:01:00 +0430

    change socket approach

--- src/Socket.ts
+++ src/Socket.ts
@@ -1,18 +1,32 @@
 import { EventEmitter } from './EventEmitter';
 import { Logger, LogType } from './logger/logger';
 import { sectionNames } from './logger/sectionNames';
+import { Config } from './index';
 import {
   Message, MessageType, stringify, newMessage, genSubscribeMessage, genUnsubscribeMessage
 } from './message';
 
 export class Socket {
-  socket: WebSocket;
+  socket: WebSocket | null = null; 
   subscribedChannels: string[];
+  path: string;
+  config: Config;
 
-  constructor(socket: WebSocket) {
-    this.socket = socket;
-    this.socket.onmessage = this.onMessage;
+  constructor(path: string, config: Config) {
+    this.path = path;
+    this.config = config;
     this.subscribedChannels = [];
+    if (!this.config || !this.config.autoReconnect) {
+      this.config.autoReconnect = true;
+    }
+  }
+
+  init = async () => {
+    this.socket = new WebSocket(this.path);
+    this.socket.onopen = this.onOpen;
+    this.socket.onmessage = this.onMessage;
+    this.socket.onclose = this.onClose;
+    this.socket.onerror = this.onError;
   }
 
   onNewMessage = (callback: (msg: string) => void): void => {
@@ -20,33 +34,33 @@ export class Socket {
   }
 
   subscribe = (channelName: string, callback: (msg: string) => void) => {
-    this.socket.send(stringify(genSubscribeMessage(channelName)));
+    this.socket!.send(stringify(genSubscribeMessage(channelName)));
     this.subscribedChannels.push(channelName);
     EventEmitter.getInstance().addListener(channelName, callback);
   }
 
   unsubscribe = (channelName: string, callback: () => void) => {
-    this.socket.send(stringify(genUnsubscribeMessage(channelName)));
+    this.socket!.send(stringify(genUnsubscribeMessage(channelName)));
     EventEmitter.getInstance().removeListener(channelName, callback);
   } 
 
   send = (msg: string): void => {
-    this.socket.send(stringify(newMessage('', msg)));
+    this.socket!.send(stringify(newMessage('', msg)));
   }
 
   publish = (channel: string, msg: string): void => {
-    this.socket.send(stringify(newMessage(channel, msg)));
+    this.socket!.send(stringify(newMessage(channel, msg)));
   }
 
   unsubscribeAll = () => {
     for (const ch of this.subscribedChannels) {
-      this.socket.send(stringify(genUnsubscribeMessage(ch)))
+      this.socket!.send(stringify(genUnsubscribeMessage(ch)))
     }
     Logger.getInstance().log(LogType.Notice, sectionNames.main, 'Closing Socket Connection...');
   }
 
   destroyConnection = () => {
-    this.socket.close();
+    this.socket!.close();
   }
 
   private onMessage = (event: MessageEvent) => {
@@ -60,6 +74,29 @@ export class Socket {
       }
     } catch (error: any) {
       Logger.getInstance().log(LogType.Error, sectionNames.socket, error.message);
+    }
+  }
+
+  private onClose = (event: Event) => {
+    if (this.config.onClose) {
+      this.config.onClose(event);
+    }
+    if (this.config.autoReconnect) {
+      setTimeout(() => {
+        this.init();
+      }, 1000);
+    }
+  }
+
+  private onError = (event: Event) => {
+    if (this.config.onError) {
+      this.config.onError(event);
+    }
+  }
+
+  private onOpen = (event: Event) => {
+    if (this.config.onOpen) {
+      this.config.onOpen(event);
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,12 @@
 import { Socket } from './Socket';
 
-interface Config {
+export interface Config {
   autoReconnect: boolean;
   onClose: (event: Event) => void;
   onError: (err: Event) => void;
   onOpen: (event: Event) => void;
 }
 
-let socket: Socket
-
-export const panda = (path: string, cfg: Config): Promise<Socket> => {
-  return new Promise((resolve, reject) => {
-    connect(path, cfg, resolve, reject);
-  });
-};
-
-const connect = (
-    path: string, 
-    conifg: Config,
-    resolve: (value: Socket | PromiseLike<Socket>) => void, 
-    reject: (reason?: any) => void
-  ) => {
-
-  const ws = new WebSocket(path);
-  socket = new Socket(ws);
-
-  ws.onopen = (event) => {
-    if (conifg.onOpen) {
-      conifg.onOpen(event);
-    }
-    resolve(socket);
-  };
-
-  ws.onerror = (event) => {
-    if (conifg.onError) {
-      conifg.onError(event);
-    }
-    reject(new Error('WebSocket could not connect. Please check your configuration.'));
-  };
-
-  ws.onclose = (event: Event) => {
-    socket.unsubscribeAll();
-    if (conifg.autoReconnect) {
-      setTimeout(() => {
-        connect(path, conifg, resolve, reject);
-      }, 1000);
-    }
-    if (conifg.onClose) {
-      conifg.onClose(event);
-    }
-  };
+export const panda = (path: string, cfg: Config): Socket => {
+  return new Socket(path, cfg);
 };
