d6092c755c5d4c8ba114f9ffbd05be5e9197a807
techerfan <techerfan@gmail.com>
2021-09-22 12:48:53 +0330

    change approach by using queue manager

--- src/Socket.ts
+++ src/Socket.ts
@@ -2,9 +2,13 @@ import { EventEmitter } from './EventEmitter';
 import { Logger, LogType } from './logger/logger';
 import { sectionNames } from './logger/sectionNames';
 import { Config } from './index';
-import {
-  Message, MessageType, stringify, newMessage, genSubscribeMessage, genUnsubscribeMessage
-} from './message';
+import { Message, MessageType, stringify, genUnsubscribeMessage } from './message';
+import { Queue } from './Queue/Queue';
+import { PublishCommand } from './Queue/PublishCommand';
+import { SocketReceiver } from './Queue/SocketReceiver';
+import { SendCommand } from './Queue/SendCommand';
+import { SubscribeCommand } from './Queue/SubscribeCommand';
+import { UnsubscribeCommand } from './Queue/UnsubsctibeCommand';
 
 interface Channel {
   name: string;
@@ -12,10 +16,12 @@ interface Channel {
 }
 
 export class Socket {
-  socket: WebSocket | null = null; 
+  socket: WebSocket | null = null;
   subscribedChannels: Channel[];
   path: string;
   config: Config;
+  private QueueManager: Queue;
+  private Receiver: SocketReceiver;
 
   constructor(path: string, config: Config) {
     this.path = path;
@@ -24,97 +30,74 @@ export class Socket {
     if (!this.config || !this.config.autoReconnect) {
       this.config.autoReconnect = true;
     }
+    this.QueueManager = Queue.getInstance();
+    this.Receiver = new SocketReceiver(this);
   }
 
-  init = (conf?: { returnSocket: boolean }): Promise<Socket> | void => {
-    this.socket = new WebSocket(this.path);
-    this.socket.onmessage = this.onMessage;
-    this.socket.onclose = this.onClose;
+  init = () => {
+    try {
+      this.socket = new WebSocket(this.path);
+      this.socket.onmessage = this.onMessage;
+      this.socket.onclose = this.onClose;
+      this.socket!.onopen = this.onOpen;
+      this.socket!.onerror = this.onError;
 
-    this.subscribeToDefinedChannels();
-
-    if ((conf && conf.returnSocket !== false) || !conf) {
-      return new Promise((resolve, reject) => {
-        this.socket!.onopen = (event) => {
-          if (this.config.onOpen) {
-            this.config.onOpen(event);
-          }
-          resolve(this);
-        }
-  
-        this.socket!.onerror = (event) => {
-          if (this.config.onError) {
-            this.config.onError(event);
-          }
-          reject(`Could not connect to ${this.path}`);
-        } 
-      });
-    } else {
-      this.socket!.onopen = (event) => {
-        if (this.config.onOpen) {
-          this.config.onOpen(event);
-        }
-      }
-
-      this.socket!.onerror = (event) => {
-        if (this.config.onError) {
-          this.config.onError(event);
-        }
-      } 
+      this.subscribeToDefinedChannels();
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   onNewMessage = (callback: (msg: string) => void): void => {
     EventEmitter.getInstance().addListener('', callback);
-  }
+  };
 
   subscribe = (channelName: string, callback: (msg: string) => void) => {
-    for (const ch of this.subscribedChannels) {
-      if (ch.name === channelName) {
-        return
-      }
-    }
-    this.socket!.send(stringify(genSubscribeMessage(channelName)));
-    this.subscribedChannels.push({
-      name: channelName,
-      callback,
-    });
-    EventEmitter.getInstance().addListener(channelName, callback);
-  }
+    this.QueueManager.add(new SubscribeCommand(this.Receiver, channelName, callback));
+  };
 
   unsubscribe = (channelName: string, callback: () => void) => {
-    this.socket!.send(stringify(genUnsubscribeMessage(channelName)));
-    this.subscribedChannels = this.subscribedChannels.filter((el, index) => {
-      return el.name !== channelName;
-    });
-    EventEmitter.getInstance().removeListener(channelName, callback);
-  } 
+    this.QueueManager.add(new UnsubscribeCommand(this.Receiver, channelName, callback));
+  };
 
   send = (msg: string): void => {
-    this.socket!.send(stringify(newMessage('', msg)));
-  }
+    this.QueueManager.add(new SendCommand(this.Receiver, msg));
+  };
 
   publish = (channel: string, msg: string): void => {
-    this.socket!.send(stringify(newMessage(channel, msg)));
-  }
+    this.QueueManager.add(new PublishCommand(this.Receiver, channel, msg));
+  };
 
-  unsubscribeAll = () => {
-    for (const ch of this.subscribedChannels) {
-      this.socket!.send(stringify(genUnsubscribeMessage(ch.name)))
-    }
-    Logger.getInstance().log(LogType.Notice, sectionNames.main, 'Closing Socket Connection...');
-  }
+  // unsubscribeAll = () => {
+  //   for (const ch of this.subscribedChannels) {
+  //     this.socket!.send(stringify(genUnsubscribeMessage(ch.name)));
+  //   }
+  // };
 
   destroyConnection = () => {
     this.socket!.close();
-  }
+  };
 
   private subscribeToDefinedChannels = () => {
     for (const ch of this.subscribedChannels) {
       // EventEmitter.getInstance().removeListener(ch.name, ch.callback);
-      EventEmitter.getInstance().addListener(ch.name, ch.callback)
+      EventEmitter.getInstance().addListener(ch.name, ch.callback);
     }
-  }
+  };
+
+  private onOpen = (event: Event) => {
+    if (this.config.onOpen) {
+      this.config.onOpen(event);
+    }
+    this.QueueManager.start();
+  };
+
+  private onError = (event: Event) => {
+    if (this.config.onError) {
+      this.config.onError(event);
+    }
+    this.QueueManager.stop();
+  };
 
   private onMessage = (event: MessageEvent) => {
     try {
@@ -128,7 +111,7 @@ export class Socket {
     } catch (error: any) {
       Logger.getInstance().log(LogType.Error, sectionNames.socket, error.message);
     }
-  }
+  };
 
   private onClose = (event: Event) => {
     if (this.config.onClose) {
@@ -136,8 +119,9 @@ export class Socket {
     }
     if (this.config.autoReconnect) {
       setTimeout(() => {
-        this.init({ returnSocket: false });
+        this.init();
       }, 1000);
     }
-  }
+    this.QueueManager.stop();
+  };
 }
